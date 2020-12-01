6fdfeec17dc6345908984d1bfb01bf9e31f444f0
Ahmed Hassan <a.hassan19995@gmail.com>
2020-12-01 08:21:34 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         case '/':
         case '%':
           str += buttonText;
+          print("string " + str);
           if (shouldCalculate) {
             _calculate();
           } else {
